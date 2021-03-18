34c6388fb8f4de7cc445afc7ed8ccf5ffcffe8fe
Gonzalo-AGM <a01702956@itesm.mx>
2021-03-18 00:54:49 -0600

    Funcionalidad de consultas completa

--- codigo/js/consultas.js
+++ codigo/js/consultas.js
@@ -22,3 +22,41 @@ document.addEventListener('DOMContentLoaded', function () {
       });
 
 M.AutoInit();
+
+var listaProg = [];
+
+function selCard(evt) {
+    if(evt.currentTarget.className.includes("blue-grey")){
+        evt.currentTarget.className = evt.currentTarget.className.replace(" blue-grey", " light-blue");
+        listaProg.push(evt.currentTarget.id);
+        console.table(listaProg);
+    }
+    else if(evt.currentTarget.className.includes("light-blue")){
+        evt.currentTarget.className = evt.currentTarget.className.replace(" light-blue", " blue-grey");
+        let pos = listaProg.indexOf(evt.currentTarget.id);
+        listaProg.splice(pos, 1);
+        console.table(listaProg);
+    }
+    if(listaProg.length > 1){
+        document.getElementById('CalifOProg').removeAttribute("disabled");
+    }else{
+        document.getElementById('CalifOProg').setAttribute("disabled",null);
+    }
+}
+
+function hacerConsulta(){
+    console.log(document.getElementById('CalifOProg').checked);
+    if(listaProg.length > 1){
+        if(document.getElementById('CalifOProg').checked){
+            location.replace("./consultasResultados1.html");
+        }else{
+            location.replace("./consultasResultados2.html");
+        }
+    }else{
+        if(document.getElementById('datosPart').checked){
+            location.replace("./consultasResultados3.html");
+        }else{
+            location.replace("./consultasResultados4.html");
+        }
+    }
+}
