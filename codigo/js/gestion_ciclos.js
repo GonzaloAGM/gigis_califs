79ca08e8ea0e5d95ff263837a791ae7977e73aa4
FinocchiaroGR <A01704007@itesm.mx>
2021-03-18 15:20:31 -0600

    Agregando gestion de ciclos.js

---
+++ codigo/js/gestion_ciclos.js
@@ -0,0 +1,18 @@
+// jQuery modal
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
+
+// jQuery collap
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+// jQuery select
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
