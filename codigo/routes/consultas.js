fbbc79a34a819587a94473118b77c1486e1a3fc9
Gonzalo-AGM <a01702956@itesm.mx>
2021-03-31 02:06:11 -0600

    paginas de resultados consultas completado sus ejs

--- codigo/routes/consultas.js
+++ codigo/routes/consultas.js
@@ -89,7 +89,80 @@ const programasConsutas = [
         ciclo: 'EM2020',
         nivel:  'looks_one',
         cantDePart: 12
+    }
+];
+
+const programasResultados = [
+    {
+        nombre: 'Lectura 1',
+        nivel:  'looks_one',
+        ciclo: 'EM2020',
+        numCoincid: 13,
+        numParticip: 19,
+        promedio: 3.5,
+        avance: 35,
+        linkFoto: 'https://www.fundacionsomos.cl/wp-content/uploads/2017/05/IMG_2792-960x750.jpeg'
     },
+    {
+        nombre: 'Lectura 1',
+        nivel:  'looks_one',
+        ciclo: 'EM2020',
+        numCoincid: 13,
+        numParticip: 19,
+        promedio: 3.5,
+        avance: 35,
+        linkFoto: 'https://www.fundacionsomos.cl/wp-content/uploads/2017/05/IMG_2792-960x750.jpeg'
+    },
+    {
+        nombre: 'Lectura 1',
+        nivel:  'looks_one',
+        ciclo: 'EM2020',
+        numCoincid: 13,
+        numParticip: 19,
+        promedio: 3.5,
+        avance: 35,
+        linkFoto: 'https://www.fundacionsomos.cl/wp-content/uploads/2017/05/IMG_2792-960x750.jpeg'
+    },
+    {
+        nombre: 'Lectura 1',
+        nivel:  'looks_one',
+        ciclo: 'EM2020',
+        numCoincid: 13,
+        numParticip: 19,
+        promedio: 3.5,
+        avance: 35,
+        linkFoto: 'https://www.fundacionsomos.cl/wp-content/uploads/2017/05/IMG_2792-960x750.jpeg'
+    },
+    {
+        nombre: 'Lectura 1',
+        nivel:  'looks_one',
+        ciclo: 'EM2020',
+        numCoincid: 13,
+        numParticip: 19,
+        promedio: 3.5,
+        avance: 35,
+        linkFoto: 'https://www.fundacionsomos.cl/wp-content/uploads/2017/05/IMG_2792-960x750.jpeg'
+    },
+    {
+        nombre: 'Lectura 1',
+        nivel:  'looks_one',
+        ciclo: 'EM2020',
+        numCoincid: 13,
+        numParticip: 19,
+        promedio: 3.5,
+        avance: 35,
+        linkFoto: 'https://www.fundacionsomos.cl/wp-content/uploads/2017/05/IMG_2792-960x750.jpeg'
+    },
+    {
+        nombre: 'Lectura 1',
+        nivel:  'looks_one',
+        ciclo: 'EM2020',
+        numCoincid: 13,
+        numParticip: 19,
+        promedio: 3.5,
+        avance: 35,
+        linkFoto: 'https://www.fundacionsomos.cl/wp-content/uploads/2017/05/IMG_2792-960x750.jpeg'
+    }
 ];
 
 const carrouselTarj = [
@@ -111,7 +184,14 @@ const carrouselTarj = [
     }
 ];
 
-let estadoConsulta = 1;
+let consultaGen = {
+    promTotal: 3.3,
+    totalCoincid: 24,
+    avanceGen: 25 
+};
+
+let estadoConsulta = true;
+let mostrarSexEdad = true;
 
 // parse application/x-www-form-urlencoded
 router.use(bodyParser.urlencoded({ extended: false }))
@@ -120,10 +200,13 @@ router.use(bodyParser.urlencoded({ extended: false }))
 router.use(express.static(path.join(__dirname,'..', 'public')));
 
 router.get('/Resultados', (request, response, next) => {
-    response.render('consultasResultados', {
+    response.render('consultas_Resultados', {
         tituloDeHeader: "Consulta - Resultados",
         tituloBarra: "Resultados de consulta",
-        resultado: estadoConsulta
+        estadoConsulta: estadoConsulta,
+        mostrarSexEdad: mostrarSexEdad,
+        consultaGen: consultaGen,
+        programasResultados: programasResultados
     });
 
     console.log("Consultas Resultados");
@@ -137,8 +220,11 @@ router.post('/Resultados', (request, response, next) => {
 });
 
 router.get('/Resultados/ProgramaN', (request, response, next) => {
-    response.render('consultasResultados', {
-        tituloDeHeader: "Resultados Programa N"
+    response.render('consultas_Programa', {
+        tituloDeHeader: "Resultados Programa N",
+        tituloBarra: "Resultados - Programa 1 - Ciclo EM 2020",
+        estadoConsulta: estadoConsulta,
+        mostrarSexEdad: mostrarSexEdad
     });
     console.log("Consultas Resultados por programa");
     response.status(201);
