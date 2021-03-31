fe4a308ec52c8423c9d631ca49eed36100bbc7d8
Gonzalo-AGM <a01702956@itesm.mx>
2021-03-31 00:23:31 -0600

    includes y pag general de consultas completado sus ejs

--- codigo/routes/consultas.js
+++ codigo/routes/consultas.js
@@ -5,6 +5,114 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 
+const programasConsutas = [
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+    {
+        nombre: 'Lectura 1',
+        ciclo: 'EM2020',
+        nivel:  'looks_one',
+        cantDePart: 12
+    },
+];
+
+const carrouselTarj = [
+    {
+        color: 'red',
+        numLink: '#one!'
+    },
+    {
+        color: 'blue',
+        numLink: '#two!'
+    },
+    {
+        color: 'green',
+        numLink: '#three!'
+    },
+    {
+        color: 'yellow',
+        numLink: '#four!'
+    }
+];
+
+let estadoConsulta = 1;
+
 // parse application/x-www-form-urlencoded
 router.use(bodyParser.urlencoded({ extended: false }))
 
@@ -13,8 +121,11 @@ router.use(express.static(path.join(__dirname,'..', 'public')));
 
 router.get('/Resultados', (request, response, next) => {
     response.render('consultasResultados', {
-        tituloDeHeader: "Resultados de Consulta"
+        tituloDeHeader: "Consulta - Resultados",
+        tituloBarra: "Resultados de consulta",
+        resultado: estadoConsulta
     });
+
     console.log("Consultas Resultados");
     response.status(201);
 });
@@ -40,13 +151,26 @@ router.post('/Resultados/ProgramaN', (request, response, next) => {
 });
 
 router.get('/', (request, response, next) => {
-    response.render('Consultas', {
-        tituloDeHeader: "Consultas"
+    response.render('consultas', {
+        tituloDeHeader: "Consultas",
+        tituloBarra: "Consultas",
+        carrouselTarj: carrouselTarj,
+        programasConsutas: programasConsutas,
+        numProg: programasConsutas.length
     });
     console.log("Consultas Resultados por programa");
     response.status(201);
 });
 
+/* //Este POST es para fechas y filtrar programas
+router.post('/', (request, response, next) => {
+    console.log("Accion post en consultas");
+    response.redirect('Consultas/Resultados');
+    response.status(302);
+});
+*/
+
+//Este POST es para filtrar los resultados de los programas existentes
 router.post('/', (request, response, next) => {
     console.log("Accion post en consultas");
     response.redirect('Consultas/Resultados');
