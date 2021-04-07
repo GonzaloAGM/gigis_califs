e8e23962c808fe78daea83f25ce4f8c44112d71d
CarlosMtz97 <A00820293@itesm.mx>
2021-04-06 23:53:12 -0500

    Se cambio la estructura de programas para un mejor manejo de las referencias

--- codigo/models/programas.js
+++ codigo/models/programas.js
@@ -3,36 +3,84 @@ const programas = [
         nombre: 'Gateo y caminata',
         ciclo: 'EM2020',
         promedio: '4.5',
-        grupos:  [1,2],
-        referencia: '/programas/programa1'
+        grupos:  [
+            {
+                numero: 1,
+                referencia: '/programas/gateo-y-caminata-grupo-1'
+            },
+            {
+                numero: 2,
+                referencia: '/programas/gateo-y-caminata-grupo-2'
+            }
+        ],
     },
     {
         nombre: 'Cocina',
         ciclo: 'EM2020',
         promedio: '4',
-        grupos: [1],
-        referencia: '/programas/programa1'
+        grupos: [
+            {
+                numero: 1,
+                referencia: '/programas/cocina-grupo-1'
+            }
+        ],
     },
     {
         nombre: 'Lectura',
         ciclo: 'EM2020',
         promedio: '3',
-        grupos:  [1,2],
-        referencia: '/programas/programa1'
+        grupos:  [
+            {
+                numero: 1,
+                referencia: '/programas/lectura-grupo-1'
+            },
+            {
+                numero: 2,
+                referencia: '/programas/lectura-grupo-2'
+            }
+        ],
     },
     {
         nombre: 'Sensorial',
         ciclo: 'EM2020',
         promedio: 'X',
-        grupos:  [1,2,3],
-        referencia: '/programas/programa1'
+        grupos:  [
+            {
+                numero: 1,
+                referencia: '/programas/sensorial-grupo-1'
+            },
+            {
+                numero: 2,
+                referencia: '/programas/sensorial-grupo-2'
+            },
+            {
+                numero: 3,
+                referencia: '/programas/sensorial-grupo-3'
+            }
+        ],
     },
     {
         nombre: 'Matemáticas',
         ciclo: 'EM2020',
         promedio: '3',
-        grupos: [1,2,3,4],
-        referencia: '/programas/programa1'
+        grupos: [
+            {
+                numero: 1,
+                referencia: '/programas/matematicas-grupo-1'
+            },
+            {
+                numero: 2,
+                referencia: '/programas/matematicas-grupo-2'
+            },
+            {
+                numero: 3,
+                referencia: '/programas/matematicas-grupo-3'
+            },
+            {
+                numero: 4,
+                referencia: '/programas/matematicas-grupo-4'
+            }
+        ],
     }
 
 ];
@@ -40,12 +88,11 @@ const programas = [
 module.exports = class Programas {
 
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(nombre, ciclo, promedio, grupos,referencia) {
+    constructor(nombre, ciclo, promedio, grupos) {
         this.nombre = nombre;
         this.ciclo = ciclo;
         this.promedio = promedio;
         this.grupos = grupos;
-        this.referencia = referencia;
     }
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
