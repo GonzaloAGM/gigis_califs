4d9081f7ebfbea2a3b84103f78587920a73bca67
CarlosMtz97 <A00820293@itesm.mx>
2021-04-07 00:17:27 -0500

    Las paginas de los grupos aparecen correctamente con los datos especificos de cada uno

--- codigo/controllers/programas_contoller.js
+++ codigo/controllers/programas_contoller.js
@@ -6,11 +6,41 @@ const programas = Programa.fetchAll();
 const arrows = Arrow.fetchAll();
 const grupos = Grupo.fetchAll();
 
-exports.programa1 = (request,response,next) => {
+exports.programaGateo1 = (request,response,next) => {
     response.render('programas_programa1', {
         tituloDeHeader: grupos[0].nombre,
         tituloBarra: grupos[0].nombre,
         grupo: grupos[0],
+        backArrow: {display: 'block', link: '/programas'},
+        forwArrow: arrows[1]
+    });
+}
+
+exports.programaGateo2 = (request,response,next) => {
+    response.render('programas_programa1', {
+        tituloDeHeader: grupos[1].nombre,
+        tituloBarra: grupos[1].nombre,
+        grupo: grupos[1],
+        backArrow: {display: 'block', link: '/programas'},
+        forwArrow: arrows[1]
+    });
+}
+
+exports.programaCocina1 = (request,response,next) => {
+    response.render('programas_programa1', {
+        tituloDeHeader: grupos[2].nombre,
+        tituloBarra: grupos[2].nombre,
+        grupo: grupos[2],
+        backArrow: {display: 'block', link: '/programas'},
+        forwArrow: arrows[1]
+    });
+}
+
+exports.programaLectura1 = (request,response,next) => {
+    response.render('programas_programa1', {
+        tituloDeHeader: grupos[3].nombre,
+        tituloBarra: grupos[3].nombre,
+        grupo: grupos[3],
         backArrow: {display: 'block', link: '/programas'},
         forwArrow: arrows[1]
     });
--- codigo/routes/programas.js
+++ codigo/routes/programas.js
@@ -8,8 +8,18 @@ router.use(express.static(path.join(__dirname,'..', 'public')));
 
 const programasController = require('../controllers/programas_contoller')
 
-router.get('/programa1', programasController.programa1);
+router.get('/gateo-y-caminata-grupo-1', programasController.programaGateo1);
+router.get('/gateo-y-caminata-grupo-2', programasController.programaGateo2);
+router.get('/cocina-grupo-1', programasController.programaCocina1);
+router.get('/lectura-grupo-1', programasController.programaLectura1);
+router.get('/lectura-grupo-2', programasController.get);
+router.get('/sensorial-grupo-1', programasController.get);
+router.get('/sensorial-grupo-2', programasController.get);
+router.get('/sensorial-grupo-3', programasController.get);
+router.get('/matematicas-grupo-1', programasController.get);
+router.get('/matematicas-grupo-2', programasController.get);
+router.get('/matematicas-grupo-3', programasController.get);
+router.get('/matematicas-grupo-4', programasController.get);
 
 router.get('/', programasController.get);
-
 module.exports = router;
