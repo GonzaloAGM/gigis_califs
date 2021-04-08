429cc7d19dc28daddf5ec997a8881b41d82cacb5
tellosss <sandratellosalinas@outlook.es>
2021-04-08 15:22:55 -0500

    Actualizacion de datos de objetivos

--- BD/estructura.sql
+++ BD/estructura.sql
@@ -19,20 +19,23 @@ ENGINE = InnoDB;
 
 CREATE TABLE `gigisplayhousebd`.`grupos` 
 (
-     `idGrupo` CHAR(4) NOT NULL , 
-     `numeroGrupo` INT NOT NULL , 
-     `idPrograma` CHAR(4) NOT NULL , 
-     `idCiclo` CHAR(4) NOT NULL , 
-     PRIMARY KEY (`idGrupo`)
+    `idGrupo` CHAR(4) NOT NULL , 
+    `numeroGrupo` INT NOT NULL , 
+    `idPrograma` CHAR(4) NOT NULL , 
+    `idCiclo` CHAR(4) NOT NULL , 
+    PRIMARY KEY (`idGrupo`),
+    FOREIGN KEY(`idPrograma`) REFERENCES `programas`(`idPrograma`)
 ) 
 ENGINE = InnoDB;
+--ALTER TABLE `grupos` ADD FOREIGN KEY(`idPrograma`) REFERENCES `programas`(`idPrograma`)
 
 CREATE TABLE `gigisplayhousebd`.`niveles` 
 (
     `idNivel` CHAR(4) NOT NULL , 
     `nombreNivel` VARCHAR(50) NOT NULL , 
     `idPrograma` CHAR(4) NOT NULL , 
-    PRIMARY KEY (`idNivel`)
+    PRIMARY KEY (`idPrograma`),
+    FOREIGN KEY(`idPrograma`) REFERENCES `programas`(`idPrograma`)
 ) 
 ENGINE = InnoDB;
 
@@ -42,7 +45,8 @@ CREATE TABLE `gigisplayhousebd`.`terapeutas`
     `titulo` VARCHAR(50) NULL , 
     `cv` VARCHAR(50) NULL , 
     `estatus` CHAR(1) NOT NULL , 
-    PRIMARY KEY (`login`)
+    PRIMARY KEY (`login`),
+    FOREIGN KEY(`login`) REFERENCES `usuarios`(`login`)
 ) 
 ENGINE = InnoDB;
 
@@ -52,7 +56,8 @@ CREATE TABLE `gigisplayhousebd`.`objetivos`
     `idObjetivo` CHAR(4) NOT NULL , 
     `descripcion` VARCHAR(200) NOT NULL , 
     `fechaRegistroObj` CHAR(1) NOT NULL , 
-    PRIMARY KEY (`idNivel`, `idObjetivo`)
+    PRIMARY KEY (`idNivel`, `idObjetivo`),
+    FOREIGN KEY(`idNivel`) REFERENCES `niveles`(`idNivel`)
 ) 
 ENGINE = InnoDB;
 
@@ -63,7 +68,8 @@ CREATE TABLE `gigisplayhousebd`.`participantes`
     `fechaNacimiento` DATE NOT NULL , 
     `edad` INT NOT NULL , 
     `telefonoPadre` INT NULL , 
-    PRIMARY KEY (`login`)
+    PRIMARY KEY (`login`),
+    FOREIGN KEY(`login`) REFERENCES `usuarios`(`login`)
 ) 
 ENGINE = InnoDB;
 
@@ -99,7 +105,9 @@ CREATE TABLE `gigisplayhousebd`.`grupos_terapeutas`
     `idGrupo` CHAR(4) NOT NULL , 
     `login` VARCHAR(50) NOT NULL , 
     `fechaAsignacion` DATE NOT NULL , 
-    PRIMARY KEY (`idGrupo`, `login`)
+    PRIMARY KEY (`idGrupo`, `login`),
+    FOREIGN KEY(`idGrupo`) REFERENCES `grupos`(`idGrupo`),
+    FOREIGN KEY(`login`) REFERENCES `terapeutas`(`login`)
 ) 
 ENGINE = InnoDB;
 
@@ -108,10 +116,35 @@ CREATE TABLE `gigisplayhousebd`.`participantes_grupos_objetivo`
     `login` VARCHAR(50) NOT NULL , 
     `idGrupo` CHAR(4) NOT NULL , 
     `idNivel` CHAR(4) NOT NULL , 
-    `idCiclo` CHAR(4) NOT NULL , 
+    `idObjetivo` CHAR(4) NOT NULL , 
     `puntajeInicial` INT NULL , 
     `puntajeFinal` INT NULL , 
-    PRIMARY KEY (`login`, `idGrupo`, `idNivel`, `idCiclo`, `puntajeInicial`)
+    PRIMARY KEY (`login`, `idGrupo`, `idNivel`, `idObjetivo`),
+    FOREIGN KEY(`login`) REFERENCES `participantes`(`login`),
+    FOREIGN KEY(`idGrupo`) REFERENCES `grupos`(`idGrupo`),
+
+) 
+ENGINE = InnoDB;
+ALTER TABLE `participantes_grupos_objetivo` 
+ADD CONSTRAINT `cfpuntajesidObjetivoobjetivos` 
+FOREIGN KEY (`idNivel`, `idObjetivo`) 
+REFERENCES `objetivos`(`idNivel`, `idObjetivo`) 
+ON DELETE RESTRICT 
+ON UPDATE RESTRICT;
+
+CREATE TABLE `gigisplayhousebd`.`participantes_grupos_objetivo` 
+( 
+    `login` VARCHAR(50) NOT NULL , 
+    `idGrupo` CHAR(4) NOT NULL , 
+    `idNivel` CHAR(4) NOT NULL , 
+    `idObjetivo` CHAR(4) NOT NULL , 
+    `puntajeInicial` INT NULL , 
+    `puntajeFinal` INT NULL , 
+    PRIMARY KEY (`login`, `idGrupo`, `idNivel`, `idObjetivo`),
+    FOREIGN KEY(`login`) REFERENCES `participantes`(`login`),
+    FOREIGN KEY(`idGrupo`) REFERENCES `grupos`(`idGrupo`),
+    FOREIGN KEY(`idNivel idObjetivo`) REFERENCES `objetivos`(`idNivel idObjetivo`)
+
 ) 
 ENGINE = InnoDB;
 
@@ -120,7 +153,9 @@ CREATE TABLE `gigisplayhousebd`.`usuarios_roles`
     `login` VARCHAR(50) NOT NULL , 
     `idRol` CHAR(4) NOT NULL , 
     `fechaUR` DATE NOT NULL , 
-    PRIMARY KEY (`login`, `idRol`)
+    PRIMARY KEY (`login`, `idRol`),
+    FOREIGN KEY(`login`) REFERENCES `usuarios` (`login`),
+    FOREIGN KEY(`idRol`) REFERENCES `roles` (`idRol`)
 ) 
 ENGINE = InnoDB;
 
@@ -129,7 +164,10 @@ CREATE TABLE `gigisplayhousebd`.`roles_funciones`
     `idRol` CHAR(3) NOT NULL , 
     `idfuncion` CHAR(3) NOT NULL , 
     `fechaRF` DATE NOT NULL , 
-    PRIMARY KEY (`idRol`, `idfuncion`)
+    PRIMARY KEY (`idRol`, `idfuncion`),
+    FOREIGN KEY(`idRol`) REFERENCES `roles` (`idRol`),
+    FOREIGN KEY(`idfuncion`) REFERENCES `funciones` (`idfuncion`)
+
 ) 
 ENGINE = InnoDB;
 
