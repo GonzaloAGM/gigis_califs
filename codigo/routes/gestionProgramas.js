253b5f8020c3a354dd615babe3d4923451412a62
tellosss <sandratellosalinas@outlook.es>
2021-04-07 20:23:07 -0500

    Agregar ejs de objetivos

--- codigo/routes/gestionProgramas.js
+++ codigo/routes/gestionProgramas.js
@@ -12,14 +12,6 @@ const arrows = [
 
 const programas = [
     {
-        nombre:     'Cocina',
-        niveles:  [
-            {
-                nombre: 'Nivel 0',
-            }
-        ],
-    },
-    {
         nombre:     'Lenguaje',
         niveles:  [
             {
@@ -33,6 +25,14 @@ const programas = [
             },
             {
                 nombre: 'Avanzado',
+            }
+        ],
+    },
+    {
+        nombre:     'Cocina',
+        niveles:  [
+            {
+                nombre: 'Nivel 1',
             }
         ],
     },
@@ -54,7 +54,7 @@ const programas = [
         nombre:     'Soy productivo',
         niveles:  [
             {
-                nombre: 'Nivel 0',
+                nombre: 'Nivel 1',
             }
         ],
     },
@@ -62,20 +62,76 @@ const programas = [
         nombre:     'Terapia sensorial',
         niveles:  [
             {
-                nombre: 'Nivel 0',
+                nombre: 'Nivel 1',
             }
         ],
     }    
 ];
 
+const objetivos = [
+    {
+        nivel:          '1',
+        numero:         '1', 
+        descripcion:    'Balbucea'
+    },
+    {
+        nivel:          '1',
+        numero:         '2', 
+        descripcion:    'Emite ruidos con su garganta'
+    },
+    {
+        nivel:          '1',
+        numero:         '3', 
+        descripcion:    'Hace pequeños ruidos cuando se le habla'
+    },
+    {
+        nivel:          '1',
+        numero:         '4', 
+        descripcion:    'Crea sonidos relacionados con el placer y el dolor'
+    },
+    {
+        nivel:          '1',
+        numero:         '5', 
+        descripcion:    'Sensibilidad ante el ruido'
+    },
+    {
+        nivel:          '2',
+        numero:         '1', 
+        descripcion:    'Pronuncia vocales'
+    },
+    {
+        nivel:          '2',
+        numero:         '2', 
+        descripcion:    'Comprende la palabra no'
+    },
+    {
+        nivel:          '2',
+        numero:         '3', 
+        descripcion:    'Conoce y responde a su nombre'
+    },
+    {
+        nivel:          '2',
+        numero:         '4', 
+        descripcion:    'Disfruta con las canciones'
+    },
+    {
+        nivel:          '2',
+        numero:         '5', 
+        descripcion:    'Surge el laleo que son sonidos vocalicos y consonanticos'
+    }
+    
+    
+];
+
 subrouter.use(bodyParser.urlencoded({ extended: false }))
 subrouter.use(express.static(path.join(__dirname,'..', 'public')));
 
-subrouter.get('/nuevo-programa', (request,response,next) => {
-    response.render('nuevo_programa', {
-        tituloDeHeader: "No sé",
-        tituloBarra: "Tampoco sé",
-        backArrow: {display: 'block', link: '/gestionProgramas'},
+subrouter.get('/gestion-nivel-objetivos', (request,response,next) => {
+    response.render('objetivos', {
+        objetivos:objetivos,
+        tituloDeHeader: "Objetivos",
+        tituloBarra: "Objetivos de lenguaje nivel 1",
+        backArrow: {display: 'block', link: '/gestionAdmin/gestionProgramas'},
         forwArrow: arrows[1]
     });
 });
@@ -90,12 +146,13 @@ subrouter.get('/', (request,response,next) => {
 });
 subrouter.post('/', (request,response,next) => {
     programas.push({
-        nombre: request.body.nombreProgra,
+        nombre:     request.body.nombreProgra,
+        niveles:
+        [{
+            nombre: request.body.nivelBase,
+        }]
     });
-    programas.niveles.push({
-        nombre: request.body.nivelBase
-    });
-    response.redirect('/gestionProgramas');
+    response.redirect('/gestionAdmin/gestionProgramas');
     console.log("Accion post en gestionProgramas");
 });
 
