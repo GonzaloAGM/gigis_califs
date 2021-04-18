77d2172966eb9e573e756af4773c0a8eb8c05b61
CarlosMtz97 <A00820293@itesm.mx>
2021-04-18 01:54:14 -0500

    Se muestran los grupos registrados en la base de datos del ciclo actual en programas

--- codigo/controllers/programas_contoller.js
+++ codigo/controllers/programas_contoller.js
@@ -2,75 +2,75 @@ const Programa = require('../models/programas');
 const Grupo = require('../models/grupos');
 const Arrow = require('../models/arrow');
 const Objetivo = require('../models/objetivos');
-const Ciclo = require('../models/ciclos');
 
 const programas = Programa.fetchAll();
 const arrows = Arrow.fetchAll();
 const grupos = Grupo.fetchAll();
 const objetivos = Objetivo.fetchAll();
 
-exports.programaGateo = (request,response,next) => {
-    const gruposGateo = [];
-    for (grupo of grupos){
-        if (grupo.nombre === 'Gateo y caminata')
-            gruposGateo.push(grupo);
-    }
+exports.programaGateo = (request, response, next) => {
+  const gruposGateo = [];
+  for (grupo of grupos) {
+    if (grupo.nombre === 'Gateo y caminata') gruposGateo.push(grupo);
+  }
 
-    response.render('programas_programa1', {
-        tituloDeHeader: gruposGateo[0].nombre,
-        tituloBarra: gruposGateo[0].nombre,
-        grupos: gruposGateo,
-        objetivos: objetivos,
-        backArrow: {display: 'block', link: '/programas'},
-        forwArrow: arrows[1]
-    });
-}
+  response.render('programas_programa1', {
+    tituloDeHeader: gruposGateo[0].nombre,
+    tituloBarra: gruposGateo[0].nombre,
+    grupos: gruposGateo,
+    objetivos: objetivos,
+    backArrow: { display: 'block', link: '/programas' },
+    forwArrow: arrows[1],
+  });
+};
 
-exports.programaCocina = (request,response,next) => {
-    const gruposCocina = [];
-    for (grupo of grupos){
-        if (grupo.nombre === 'Cocina')
-            gruposCocina.push(grupo);
-    }
-    response.render('programas_programa1', {
-        tituloDeHeader: gruposCocina[0].nombre,
-        tituloBarra: gruposCocina[0].nombre,
-        grupos: gruposCocina,
-        objetivos: objetivos,
-        backArrow: {display: 'block', link: '/programas'},
-        forwArrow: arrows[1]
-    });
-}
+exports.programaCocina = (request, response, next) => {
+  const gruposCocina = [];
+  for (grupo of grupos) {
+    if (grupo.nombre === 'Cocina') gruposCocina.push(grupo);
+  }
+  response.render('programas_programa1', {
+    tituloDeHeader: gruposCocina[0].nombre,
+    tituloBarra: gruposCocina[0].nombre,
+    grupos: gruposCocina,
+    objetivos: objetivos,
+    backArrow: { display: 'block', link: '/programas' },
+    forwArrow: arrows[1],
+  });
+};
 
-exports.programaLectura = (request,response,next) => {
-    const gruposLectura = [];
-    for (grupo of grupos){
-        if (grupo.nombre === 'Lectura')
-            gruposLectura.push(grupo);
-    }
-    response.render('programas_programa1', {
-        tituloDeHeader: gruposLectura[0].nombre,
-        tituloBarra: gruposLectura[0].nombre,
-        grupos: gruposLectura,
-        objetivos:objetivos,
-        backArrow: {display: 'block', link: '/programas'},
-        forwArrow: arrows[1]
-    });
-}
+exports.programaLectura = (request, response, next) => {
+  const gruposLectura = [];
+  for (grupo of grupos) {
+    if (grupo.nombre === 'Lectura') gruposLectura.push(grupo);
+  }
+  response.render('programas_programa1', {
+    tituloDeHeader: gruposLectura[0].nombre,
+    tituloBarra: gruposLectura[0].nombre,
+    grupos: gruposLectura,
+    objetivos: objetivos,
+    backArrow: { display: 'block', link: '/programas' },
+    forwArrow: arrows[1],
+  });
+};
 
-exports.get = (request,response,next) => {
-    Ciclo.fetchIdCiclo('2020-10-30')
-        .then(([rows, fieldData]) => {
-            console.log(rows);
+exports.get = (request, response, next) => {
+  Grupo.fetchProgramasCicloActual()
+    .then(([rows1, fieldData1]) => {
+      Grupo.fetchGruposCicloActual()
+        .then(([rows2, fieldData2]) => {
+          console.log(rows1);
+          console.log(rows2);
+          response.render('programas', {
+            tituloDeHeader: 'Programas',
+            tituloBarra: 'Programas',
+            programas: rows1,
+            grupos: rows2,
+            backArrow: arrows[0],
+            forwArrow: arrows[1],
+          });
         })
-        .catch(err => {
-            console.log(err);
-        });
-    response.render('programas', {
-        tituloDeHeader: "Programas",
-        tituloBarra: "Programas",
-        programas: programas,
-        backArrow: arrows[0],
-        forwArrow: arrows[1]
-    });
-}
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+};
--- codigo/models/grupos.js
+++ codigo/models/grupos.js
@@ -1,173 +1,35 @@
-const grupos = [
-    {
-        nombre: 'Gateo y caminata',
-        terapeuta: 'Valentina Saracho Pitol',
-        numero: 1,
-        promedio: 4.5,
-        participantes: [
-            {
-                nombre: 'Alan Eduardo Anaya Flores',
-                calificacion: 3,
-                nivel: 1
-            },
-            {
-                nombre: 'Alexander Hernández Vargas',
-                calificacion: 2,
-                nivel: 1
-            },
-            {
-                nombre: 'Diana Canul Pelayo',
-                calificacion: 5,
-                nivel: 1
-            }
-            
-        ]
-        
-    },
-    {
-        nombre: 'Gateo y caminata',
-        terapeuta: 'Valentina Saracho Pitol',
-        numero: 2,
-        promedio: 3,
-        participantes: [
-            {
-                nombre: 'Keyli Milagros Arias Aguillón',
-                calificacion: 3,
-                nivel: 1
-            },
-            {
-                nombre: 'Valentina González Escobar',
-                calificacion: 2,
-                nivel: 1
-            },
-            {
-                nombre: 'Orlando Mendoza Castañedas',
-                calificacion: 1,
-                nivel: 1
-            }
-            
-        ]
-        
-    },
-    {
-        nombre: 'Cocina',
-        terapeuta: 'Alejandra González',
-        numero: 1,
-        promedio: 3.5,
-        participantes: [
-            {
-                nombre: 'Diego Barrón Martínez',
-                calificacion: 5,
-                nivel: 1
-            },
-            {
-                nombre: 'María de Jesús Medina Arreguín',
-                calificacion: 5,
-                nivel: 1
-            },
-            {
-                nombre: 'Aranza Solórzano Watson',
-                calificacion: 4,
-                nivel: 1
-            },        
-            {
-                nombre: 'Ángel Miguel Rodríguez Franco',
-                calificacion: 3,
-                nivel: 1
-            }
-        ]
-        
-    },
-    {
-        nombre: 'Lectura',
-        terapeuta: 'Paula Tornell Pantoja',
-        numero: 1,
-        promedio: 3,
-        participantes: [
-            {
-                nombre: 'Isaac Delgado Nieves',
-                calificacion: 4,
-                nivel: 1
-            },
-            {
-                nombre: 'Maria Ximena Pastenes Camacho',
-                calificacion: 5,
-                nivel: 2
-            },
-            {
-                nombre: 'Kaylani Avalos Irenia ',
-                calificacion: 4,
-                nivel: 3
-            },
-            {
-                nombre: 'Franco Vázquez',
-                calificacion: 3,
-                nivel: 3
-            },
-            {
-                nombre: 'Diana Canul Pelayo ',
-                calificacion: 3,
-                nivel: 2
-            }
-        ]
-        
-    },
-    {
-        nombre: 'Lectura',
-        terapeuta: 'Paula Tornell Pantoja',
-        numero: 2,
-        promedio: 3,
-        participantes: [
-            {
-                nombre: 'Isaac Delgado Nieves',
-                calificacion: 3,
-                nivel: 1
-            },
-            {
-                nombre: 'Maria Ximena Pastenes Camacho',
-                calificacion: 3,
-                nivel: 2
-            },
-            {
-                nombre: 'Kaylani Avalos Irenia ',
-                calificacion: 3,
-                nivel: 3
-            },
-            {
-                nombre: 'Franco Vázquez',
-                calificacion: 3,
-                nivel: 3
-            },
-            {
-                nombre: 'Diana Canul Pelayo ',
-                calificacion: 3,
-                nivel: 2
-            }
-        ]
-        
-    }
-
-];
+const db = require('../util/database');
 
 module.exports = class Grupo {
+  //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
+  constructor(numeroGrupo, idPrograma, idCiclo) {
+    this.numeroGrupo = numeroGrupo;
+    this.idPrograma = idPrograma;
+    this.idCiclo = idCiclo;
+  }
 
-    //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(nombre, terapeuta, numero, promedio, participantes,) {
-        this.nombre = nombre;
-        this.terapeuta = terapeuta;
-        this.numero = numero;
-        this.promedio = promedio;
-        this.participantes = participantes;
-    }
+  //Este método servirá para guardar de manera persistente el nuevo objeto.
+  save() {
+    return db.execute(
+      'INSERT INTO grupos (numeroGrupo, idPrograma, idCiclo) VALUES (?,?,?)',
+      [this.numeroGrupo, this.idPrograma, this.idCiclo]
+    );
+  }
 
-    //Este método servirá para guardar de manera persistente el nuevo objeto. 
-    save() {
-        
-    }
+  //Este método servirá para devolver los objetos del almacenamiento persistente.
+  static fetchAll() {
+    return db.execute('SELECT * FROM grupos');
+  }
 
-    //Este método servirá para devolver los objetos del almacenamiento persistente.
-    static fetchAll() {
-        return grupos;
-    }
+  static fetchProgramasCicloActual() {
+    return db.execute(
+      'SELECT G.idPrograma, nombrePrograma, DATE_FORMAT(fechaInicial, "%M") AS fechaInicio , DATE_FORMAT(fechafinal, "%M %Y") AS fechaFinal FROM grupos G ,ciclos C, programas P WHERE G.idCiclo=C.idCiclo AND G.idPrograma=P.idPrograma AND fechaInicial<CURRENT_DATE AND fechaFinal>CURRENT_DATE GROUP BY idPrograma'
+    );
+  }
 
-}
+  static fetchGruposCicloActual() {
+    return db.execute(
+      'SELECT numeroGrupo,G.idPrograma FROM grupos G ,ciclos C, programas P WHERE G.idCiclo=C.idCiclo AND G.idPrograma=P.idPrograma AND fechaInicial<CURRENT_DATE AND fechaFinal>CURRENT_DATE'
+    );
+  }
+};
