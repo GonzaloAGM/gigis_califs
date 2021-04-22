a5ad9987b5266763789b33371ff81cc3a9e11369
tellosss <sandratellosalinas@outlook.es>
2021-04-22 14:47:47 -0500

    Agregar modelo de terapeutas

---
+++ codigo/models/terapeutas.js
@@ -0,0 +1,25 @@
+const db = require('../util/database');
+
+module.exports = class Terapeuta{
+  //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
+  constructor(login, titulo, cv, estatus) {
+    this.login = login;
+    this.titulo = titulo;
+    this.cv = cv;
+    this.estatus = estatus;
+}
+
+  //Este método servirá para guardar de manera persistente el nuevo objeto.
+  save() {
+    return db.execute(
+        'INSERT INTO terapeutas (login,titulo, cv ) VALUES (?, ?, ?)',
+        [this.login ,this.titulo,this.cv, this.estatus, 'activo']
+    );
+  }
+
+  //Este método servirá para devolver los objetos del almacenamiento persistente.
+  static fetchAll() {
+    return db.execute('SELECT * FROM participantes');
+  }
+
+}
