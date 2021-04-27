da83d3344900d00c33bf180ce33a58a596c161ab
tellosss <sandratellosalinas@outlook.es>
2021-04-27 14:11:41 -0500

    Validar que la entrada de la fecha final del ciclo está correcta

--- codigo/public/js/gestion_ciclos.js
+++ codigo/public/js/gestion_ciclos.js
@@ -13,21 +13,106 @@ $(document).ready(function(){
     $('select').formSelect();
   });
 
-// jQuery date picker 
-  $(document).ready(function(){
-    $('.datepicker').datepicker({ 
-      firstDay: true, 
-      yearRange: 2,
-      //En español
-      i18n: {
-          months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-          monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Dic"],
-          weekdays: ["Domingo","Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
-          weekdaysShort: ["Dom","Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
-          weekdaysAbbrev: ["D","L", "M", "M", "J", "V", "S"]
-      }
-  })
- });  
+// Spanish overwrite
+inter_es = {
+	cancel: "Cancelar",
+	done: "Ok",
+	months: [
+		"Enero",
+		"Febrero",
+		"Marzo",
+		"Abril",
+		"Mayo",
+		"Junio",
+		"Julio",
+		"Agosto",
+		"Septiembre",
+		"Octubre",
+		"Noviembre",
+		"Diciembre"
+	],
+	monthsShort: [
+		"Ene",
+		"Feb",
+		"Mar",
+		"Abr",
+		"May",
+		"Jun",
+		"Jul",
+		"Ago",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dic"
+	],
+	weekdays: [
+		"Domingo",
+		"Lunes",
+		"Martes",
+		"Miércoles",
+		"Jueves",
+		"Viernes",
+		"Sábado"
+	],
+	weekdaysShort: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"],
+	weekdaysAbbrev: ["D", "L", "M", "M", "J", "V", "S"]
+};
 
- 
-  
+// Create default dates
+var date = new Date();
+// set default date for #from (1 week from today)
+var nextWeekFrom = new Date(date.setDate(date.getDate() + 7));
+// Default date for #to
+var nextWeekTo = new Date(date.setDate(nextWeekFrom.getDate() + 7));
+//Set min date for #to
+var minDateTo = new Date(date.setDate(nextWeekFrom.getDate() + 1));
+
+
+// SET OPTIONS FOR FROM DATEPICKER
+var optionsFrom = {
+  format: 'yyyy-mm-dd',
+	i18n: inter_es,
+	defaultDate: new Date(nextWeekFrom),
+	setDefaultDate: true,
+	autoClose: true,
+	onSelect: function(el) {
+		const ell = new Date(el);
+		const setMM = ell.getDate() + 1;
+		const setM = new Date(ell.setDate(setMM));
+		setMinTo(setM);
+	}
+};
+
+
+// SET OPTIONS FOR TO DATEPICKER
+var optionsTo = {
+  format: 'yyyy-mm-dd',
+	i18n: inter_es,
+	minDate: new Date(minDateTo),
+	defaultDate: new Date(nextWeekTo),
+	setDefaultDate: true,
+	autoClose: true
+};
+
+
+// INITIATE DATEPICKERS
+$(document).ready(function() {
+	var $from = $("#from").datepicker(optionsFrom);
+	var $to = $("#to").datepicker(optionsTo);
+});
+
+
+// FUNCTION TO SET MINIMUM DATE WHEN FROM DATE SELECTED
+var setMinTo = function(vad) {
+	// Get the current date set on #to datepicker
+	var instance = M.Datepicker.getInstance($("#to"));
+	instance.options.minDate = vad;
+
+	// Check if the #from date is greater than the #to date and modify by 1 day if true.
+	if (new Date(instance) < vad) {
+		instance.setDate(vad);
+		$("#to").val(instance.toString());
+	}
+};
+
+$("select").formSelect();
