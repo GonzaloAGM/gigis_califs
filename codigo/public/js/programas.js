7c78d8d4f66a4e4eca87c5408232a30389998fbf
CarlosMtz97 <A00820293@itesm.mx>
2021-05-03 00:41:46 -0500

    Se completó la tabla con puntajes sin que aparezcan visualmente

--- codigo/public/js/programas.js
+++ codigo/public/js/programas.js
@@ -25,8 +25,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems);
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('select');
+var instances = M.FormSelect.init(elems);
+});
 
